93dc5bc51be0452082cdcfc2f2a62ea49c757485
Daniel <bilibip@free.fr>
2020-09-20 22:51:12 +0200

    Update file to github

---
+++ src/components/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Daniel RIVIERE',
+        username: 'DanRiv',
+        email: 'bilibip@free.fr'
+    },
+    {
+        id: 2,
+        name: 'Lea RIVIERE',
+        username: 'LeaRiv',
+        email: 'leariv@free.fr'
+    },
+    {
+        id: 3,
+        name: 'Enzo RIVIERE',
+        username: 'EnzRiv',
+        email: 'enzoriviere@free.fr'
+    },
+    {
+        id: 4,
+        name: 'Aurélie GIMENEZ',
+        username: 'AureGIM',
+        email: 'aureliegimenez@free.fr'
+    },
+    {
+        id: 5,
+        name: 'Delphine GIMENEZ',
+        username: 'DelGIM',
+        email: 'delphinegimenez@free.fr'
+    },
+]
